fb8e6ed1e05c7cf2cd0346a3bd51ae3298008a95
Iknow021 <163538185+uZwide@users.noreply.github.com>
2024-06-26 09:58:15 +0200

    clean up imports in main file

--- src/main/java/com/nkosi/programallocator/PlaygroundApplication.java
+++ src/main/java/com/nkosi/programallocator/PlaygroundApplication.java
@@ -1,12 +1,10 @@
 package com.nkosi.programallocator;
 
-//import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
-//@EnableEncryptableProperties
 @SpringBootApplication
 public class PlaygroundApplication {
 
